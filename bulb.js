604a3f9b6fbfdec54f7dc7c92b598cefd13b4b87
SURNI KURMA RAO <kurmaraosurni@gmail.com>
2025-09-15 21:44:42 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,8 @@
+// JavaScript code can be added here if needed
+    
+    function on() {
+        document.getElementsByTagName("img")[0].src="Screenshot 2025-09-15 211727.png"
+    }
+    function off() {
+        document.getElementsByTagName("img")[0].src="Screenshot 2025-09-15 213207.png"
+    }
